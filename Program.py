e3d601baa03d297de6fc22df42f261312019ec31
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-10-13 22:35:54 +0530

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,1 +1,14 @@
+###   Make My Trip   ###
+a = input("What is Your Name:: \n")
+#]print("Helllo",a)
+def Taxi_cost(Airport):
+  if Airport=="Domestic":
+    return 1500
+  elif Airport=="International":
+    return 2500
+  else:
+    print("Sorry we Dont Have Data For:: \n",Airport)   
+    return 0 
+Airport= input("Which Airport are you Going:: \n")
+print("Taxi cost for" ,Airport ,"is",Taxi_cost(Airport))
 
